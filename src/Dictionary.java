d303daf48a791a0743ffa2cfe16ed7a46a234c21
yamna-ali <yamna_fasih.92@hotmail.com>
2020-08-28 10:02:28 -0400

    first commit

---
+++ src/Dictionary.java
@@ -0,0 +1,44 @@
+/*You are to a write a program which asks a user to enter 10 words.
+Save these words in an array.  Using the bubble sort algorithm (see below),
+ sort the words in an ascending order. For the output, print out the words in the original order,
+ in the ascending order, and in the descending order.
+ Finally, print an array where all letters have been changed to uppercase.
+  Don't forget to write a pseudocode or flowchart BEFORE writing your code.
+ */
+
+
+import java.util.Scanner;
+
+public class Dictionary {
+public static void main(String[] args){
+     Scanner scanner= new Scanner(System.in);
+    System.out.println("Enter 10 words: ");
+    String userInput = scanner.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
